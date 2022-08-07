76af86ed70a8f149cbacd38a40137a6139af805d
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-07 19:52:19 -0300

    update resolvers

--- index.ts
+++ index.ts
@@ -3,11 +3,17 @@ import path from 'path';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import { QuestResolver } from './src/resolvers/QuestResolver';
+import { CategoryResolver } from './src/resolvers/CategoryResolver';
+import { AlternativeResolver } from './src/resolvers/AlternativeResolver';
+import { MergeCategoryResolver } from './src/resolvers/MergeCategoryResolver';
 
 async function main(){
   const schema = await buildSchema({
     resolvers: [
       QuestResolver,
+      CategoryResolver,
+      AlternativeResolver,
+      MergeCategoryResolver
     ],
     emitSchemaFile: path.resolve(__dirname, 'schema.gql'),
   })
