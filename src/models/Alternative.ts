4eab46e9ea523bcbb83943229e4b10dd7798cc5f
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-07 19:48:31 -0300

    add alternative

---
+++ src/models/Alternative.ts
@@ -0,0 +1,22 @@
+import { Max, Min } from "class-validator";
+import { Field, ID, Int, ObjectType } from "type-graphql";
+
+@ObjectType()
+export class Alternative{
+  @Field(type => ID)
+  id: number;
+
+  @Field(type => String)
+  description: string;
+
+  @Field(type => Int)
+  @Min(1)
+  @Max(5)
+  level: number;
+
+  @Field(type => Int)
+  idCategory: number;
+
+  @Field(type => Int)
+  idQuest: number;
+}
