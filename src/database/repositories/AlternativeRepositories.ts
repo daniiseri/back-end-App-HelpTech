7370401251c704fa5c6106ba9583ba308fd3c167
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-09 18:19:06 -0300

    add  method findById && create

--- src/database/repositories/AlternativeRepositories.ts
+++ src/database/repositories/AlternativeRepositories.ts
@@ -7,4 +7,19 @@ export class AlternativeRepositories{
     const alternatives = await conn.execute(query);
     return alternatives;
   }
+
+  async findById(code: any){
+    const conn = await connectToMySql();
+    const query = 'SELECT * FROM alternative WHERE id = ?';
+    const alternative = await conn.execute(query, [code]);
+    return alternative;
+  }
+
+  async create(data: any){
+    const conn = await connectToMySql();
+    const query = 'INSERT INTO alternative (description, level, idCategory, idQuest) VALUES (?, ?, ?, ?)';
+    const create  = await conn.execute(query, [data.newAlternativeData.description, data.newAlternativeData.level, data.idCategory, data.idQuest]);
+    const alternative = await this.findById(create[0].insertId);
+    return alternative[0];
+  }
 }
