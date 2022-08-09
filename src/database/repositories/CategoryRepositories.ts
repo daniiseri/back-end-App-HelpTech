b9a10c15b978ab5bec37352d5423b0db22566d08
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-09 18:19:12 -0300

    add  method findById && create

--- src/database/repositories/CategoryRepositories.ts
+++ src/database/repositories/CategoryRepositories.ts
@@ -7,4 +7,20 @@ export class CategoryRepositories{
     const categories = await conn.execute(query);
     return categories;
   }
+
+  async findById(code: any){
+    const conn = await connectToMySql();
+    const query = 'SELECT * FROM category WHERE id = ?';
+    const category = await conn.execute(query, [code]);
+    return category;
+  }
+
+  async create(data: any){
+    const conn = await connectToMySql();
+    const query = 'INSERT INTO category (description) VALUES (?)';
+    const create  = await conn.execute(query, [data.description]);
+    const category = await this.findById(create[0].insertId);
+    
+    return category[0];
+  }
 }
