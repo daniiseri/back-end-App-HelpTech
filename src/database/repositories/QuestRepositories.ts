6614db19db8cb9490214966febe3ce05d738a5a3
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-09 18:19:18 -0300

    add  method findById && create

--- src/database/repositories/QuestRepositories.ts
+++ src/database/repositories/QuestRepositories.ts
@@ -1,10 +1,26 @@
 import { connectToMySql } from "../index";
 
 export class QuestRepositories{
+
   async findAll(){
     const conn = await connectToMySql();
     const query = 'SELECT * FROM quest';
     const quests = await conn.execute(query);
     return quests;
+  }
+
+  async findById(code: any){
+    const conn = await connectToMySql();
+    const query = 'SELECT * FROM quest WHERE id = ?';
+    const quests = await conn.execute(query, [code]);
+    return quests;
+  }
+
+  async create(data: any){
+    const conn = await connectToMySql();
+    const query = 'INSERT INTO quest (description, idCategory) VALUES (?, ?)';
+    const create  = await conn.execute(query, [data.newQuestData.description, data.idCategory]);
+    const quest = await this.findById(create[0].insertId);
+    return quest[0];
   }
 }
