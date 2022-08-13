be1b66b03b07129afddf3b479c69d67c3fbb9423
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-13 16:56:05 -0300

    update

--- src/database/repositories/UserRepositories.ts
+++ src/database/repositories/UserRepositories.ts
@@ -20,7 +20,6 @@ export class UserRepositories{
     const query = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)';
     const create  = await conn.execute(query, [data.name, data.email, data.password]);
     const user = await this.findById(create[0].insertId);
-    console.log(user[0]);
     return user[0];
   }
 }
