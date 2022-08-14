514aeb5fb8f3df98f04b64719a2304451b510d9f
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-14 16:39:29 -0300

    add object login

---
+++ src/models/Login.ts
@@ -0,0 +1,23 @@
+import { Field, InterfaceType, ObjectType } from "type-graphql";
+import { User } from "./User";
+
+@ObjectType()
+export class Message{
+  @Field({nullable: true})
+  user: User;
+
+  @Field({nullable: true})
+  token: string;
+
+  @Field({nullable: true})
+  error: string;
+}
+
+@ObjectType()
+export class Login extends Message{
+  @Field()
+  result: string;
+
+  @Field()
+  message: Message;
+}
