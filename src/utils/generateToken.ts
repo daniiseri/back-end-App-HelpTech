8b2909d40d6642aaad93ad4a7800bcf9e4b29d58
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-08-22 14:37:12 -0300

    generate token

---
+++ src/utils/generateToken.ts
@@ -0,0 +1,8 @@
+import jwt from 'jsonwebtoken';
+const authConfig = require('../config/auth');
+
+export const generateToken = (params:{}) => {
+  return (jwt.sign(params , authConfig.secret, {
+    expiresIn: 86400
+  }));
+}
