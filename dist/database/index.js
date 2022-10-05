f9b737619c58f9ef5d6e77e85c850a0ef60c8a63
Daniel de Souza Iseri <danielsi-2015@hotmail.com>
2022-10-05 20:51:28 -0300

    credentials

--- dist/database/index.js
+++ dist/database/index.js
@@ -3,11 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
 const connectToPostgres = async () => {
     const client = new pg_1.Client({
-        host: process.env.PGHOST,
-        database: process.env.PGDATABASE,
-        user: process.env.PGUSER,
-        port: Number(process.env.PGPORT),
-        password: process.env.PGPASSWORD,
+        connectionString: process.env.DATABASE_URL
     });
     await client.connect();
     console.log('Database running!');
--- src/database/index.ts
+++ src/database/index.ts
@@ -4,19 +4,15 @@ import { Client } from 'pg';
 const connectToPostgres = async()=>{
   const client = new Client(
     {
-      host: process.env.PGHOST,
-      database: process.env.PGDATABASE,
-      user: process.env.PGUSER,
-      port: Number(process.env.PGPORT),
-      password: process.env.PGPASSWORD,
+      connectionString: process.env.DATABASE_URL
     }
   );
-
+  
   await client.connect();
 
   console.log('Database running!');
   
-  return client
+  return client;
 }
 
 connectToPostgres()
